829e056d013bbd88790c618b1b44506b0cc866c2
Mohammed Jawad Saleem <mohammedjawadsaleem17@gmail.com>
2024-05-09 15:12:31 +0530

    Added Readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import  { useState } from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Output from './components/Output';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [values, setValues] = useState('');
+  const [result, setResult] = useState([]);
+
+  const notify = () => toast.success("Success!");
+  const notifyError = () => toast.error("You can't submit empty values!");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(values.length===0){
+      console.log(`lenght is 0`)
+      notifyError()
+      return 
+    }
+    notify()
+    console.log(`These are your initiial values: `,values.length)
+    const valuesArray = values.split(',').map(value => value.trim());
+    console.log(`This is the values Array after converting a string into a List`,valuesArray)
+
+    const response = await fetch('http://localhost:5000/fizzbuzz', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ values: valuesArray }),
+    });
+
+    const data = await response.json();
+    console.log(`The Received Data is `,data[0])  //We have the received data inthe form of a json utilizing this to Implement the UI
+    setResult(data);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Header/>
+      <Form handleSubmit={handleSubmit} values={values} setValues={setValues}/>
+      <Output result={result}/>
+      <ToastContainer />
+    </div>
+  );
 }
 
-export default App
+export default App;
